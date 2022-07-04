2fa14f3228a545e2c78e35439c7b55a10edd75bf
David Grace <davidgrace00@gmail.com>
2022-07-04 20:20:42 +0100

    Commit project

---
+++ Libraries/MyBlazorShop.Libraries.Services/Product/Models/IProductAddToCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlazorShop.Libraries.Services.Product.Models
+{
+    /// <summary>
+    /// The information needed in a product listing Razor component
+    /// </summary>
+    public interface IProductAddToCart
+    {
+        // An instance of the product
+        ProductModel? Product { get; set; }
+
+        // The quantity wishing to be added to the cart
+        int? Quantity { get; set; }
+
+        /// <summary>
+        /// The method to add a product to cart
+        /// </summary>
+        void AddToCart();
+    }
+}
---
+++ Libraries/MyBlazorShop.Libraries.Services/Product/Models/ProductModel.cs
@@ -0,0 +1,65 @@
+﻿namespace MyBlazorShop.Libraries.Services.Product.Models
+{
+    /// <summary>
+    /// Stores a product.
+    /// </summary>
+    public class ProductModel
+    {
+        /// <summary>
+        /// Unique identifier of the product.
+        /// </summary>
+        public string Sku { get; }
+
+        /// <summary>
+        /// Name of the product.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Price of the product.
+        /// </summary>
+        public decimal Price { get; }
+
+        /// <summary>
+        /// The image path of the product.
+        /// </summary>
+        public string Image { get; }
+
+        /// <summary>
+        /// The route slug of the product.
+        /// </summary>
+        public string Slug
+        {
+            get
+            {
+                return Sku.ToLower();
+            }
+        }
+
+        /// <summary>
+        /// The full URL of the product
+        /// </summary>
+        public string FullUrl
+        {
+            get
+            {
+                return string.Format("/product/{0}", Slug);
+            }
+        }
+
+        /// <summary>
+        /// Constructs a new product.
+        /// </summary>
+        /// <param name="sku">Unique identifier of the product.</param>
+        /// <param name="name">Name of the product.</param>
+        /// <param name="price">Price of the product.</param>
+        /// <param name="image">Image path of the product.</param>
+        public ProductModel(string sku, string name, decimal price, string image)
+        {
+            Sku = sku;
+            Name = name;
+            Price = price;
+            Image = image;
+        }
+    }
+}
