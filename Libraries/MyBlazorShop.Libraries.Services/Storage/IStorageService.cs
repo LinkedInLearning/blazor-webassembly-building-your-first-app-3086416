1c8ebcbed737e1a1cf22f9e22940b37ceb6b38ae
David Grace <davidgrace00@gmail.com>
2022-07-04 20:12:49 +0100

    Commit project

---
+++ Libraries/MyBlazorShop.Libraries.Services/Storage/IStorageService.cs
@@ -0,0 +1,22 @@
+﻿using MyBlazorShop.Libraries.Services.Product.Models;
+using MyBlazorShop.Libraries.Services.ShoppingCart.Models;
+
+namespace MyBlazorShop.Libraries.Services.Storage
+{
+    /// <summary>
+    /// Stores the data used for the application.
+    /// </summary>
+    public interface IStorageService
+    {
+        /// <summary>
+        /// Stores a list of products.
+        /// </summary>
+        IList<ProductModel> Products { get; }
+
+        /// <summary>
+        /// Stores the shopping cart.
+        /// </summary>
+        ShoppingCartModel ShoppingCart { get; }
+
+    }
+}
